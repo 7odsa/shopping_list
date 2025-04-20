d387adcc56909aa48ce8c0d4c2768e2c1d3af82b
Meshooo <mahmoudabdaltwab57@gmail.com>
2025-04-20 18:57:29 +0200

    Initialize shopping list App

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:shopping_list/screens/home_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(home: const MainApp()));
+  runApp(const MaterialApp(home: MainApp()));
 }
